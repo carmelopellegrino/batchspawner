1e8fc49b0eea032269c9fbca4847b0608776bbd6
Carmelo Pellegrino <carmelo.pellegrino@cnaf.infn.it>
2026-03-25 11:25:56 +0100

    add the id to radio buttons

--- batchspawner/batchspawner.py
+++ batchspawner/batchspawner.py
@@ -949,7 +949,7 @@ Queue
             groups = [g[1]['cn'][0].decode() for g in res]
 
             def make_item(g, idx, additional_tags=''):
-                return f"""<input type="radio" name="unix_group_{g}" {additional_tags} value="{idx}">
+                return f"""<input type="radio" id="unix_group_{g} name="unix_group_{g}" {additional_tags} value="{idx}">
 <label for="unix_group_{g}">{g}</label>"""
 
             if len(groups) == 1:
